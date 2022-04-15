c53137d726fb3da582750af6473c92e439d3a67a
Ochwada <Ochwada@users.noreply.github.com>
2022-04-15 22:59:36 +0200

    where, and, or, oparator precedence, in, not in, like, not like, between, null

--- 15042022_SelectStatement_sql_mysqlForDataAnalyticsAndBusinessIntelligence.sql
+++ 15042022_SelectStatement_sql_mysqlForDataAnalyticsAndBusinessIntelligence.sql
@@ -18,10 +18,41 @@ select * from employees where hire_date LIKE ('%2000%'); -- employees hired in 2
 
 select * from employees where emp_no LIKE ('1000_'); -- Employees with emp_no starting with 1000 but containing 5 digits
 
-select * from employees where first_name like ('%Jack%');
+select * from employees where first_name like ('%Jack%'); -- Employees with names containing 'jack'
 
-select * from employees where first_name NOT LIKE ('%jack%');
+select * from employees where first_name NOT LIKE ('%jack%'); -- Employees with names not containing 'Jack'
 
-select * from employees where hire_date between '1990-01-01' and '2000-01-01';
+select * from employees where hire_date between '1990-01-01' and '2000-01-01'; -- Employees hired between the said date
 
-select * from employees where hire_date not between '1990-01-01' and '2000-01-01';
+select * from employees where hire_date not between '1990-01-01' and '2000-01-01'; -- employees hired outside the said dates
+
+select * from salaries where salary between 66000 and 70000; -- salaries between the said values
+
+select  * from employees where emp_no not between 10004 and 10012; -- individuals whose employee number is not between ‘10004’ and ‘10012’
+
+select dept_name from departments where dept_no between  'd003' and 'd006'; -- names of all departments with numbers between ‘d003’ and ‘d006’. 
+
+select * from employees where first_name is not null; -- employ not missing first name 
+
+select * from employees where first_name is null; -- employ  missing first name
+
+select * from departments;
+
+select dept_name from departments where dept_no is not null; --  names of all departments whose department number value is not null
+
+select * from employees where first_name <> 'mark';  -- list of employees whose first name is not Mark
+
+select * from employees where first_name != 'mark'; -- list of employees whose first name is not Mark
+
+select * from employees where hire_date >= '2000-01-01' and gender ="F" ; -- a list with data about all female employees who were hired in the year 2000 or after.
+
+select * from salaries where salary > 15000; -- a list with all employees’ salaries higher than $150,000 per annum.
+
+select distinct gender from employees; --  distinct values in a row
+
+select * from employees;
+
+select distinct hire_date from employees;
+
+## Aggregate function
+-- They are applied on multiple rows of a single column of a table return an output of single value. 
