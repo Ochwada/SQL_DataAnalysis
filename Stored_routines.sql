0a50f931f4f51b1e0dce8d5fcda7353ef31321a6
Ochwada <Ochwada@users.noreply.github.com>
2022-04-30 23:44:10 +0200

    Merge branch 'main' of https://github.com/Ochwada/SQL_DataAnalysis

--- Stored_routines.sql
+++ Stored_routines.sql
@@ -101,3 +101,101 @@ SELECT EMP_INFO('Aruna', 'Journel');
 --  MySQL Variables --> Local,  Session, global 
 -- Local -- Variable visible only in the BEGIN - END block in which it was created.
 
+
+-- Create a trigger that checks if the hire date of an employee is higher than the current date. 
+-- If true, set this date to be the current date. Format the output appropriately (YY-MM-DD).
+DELIMITER $$ 
+CREATE TRIGGER trig_hire_date BEFORE
+	INSERT ON employees 
+		FOR EACH ROW BEGIN IF NEW.hire_date > date_format(sysdate(),
+		'%Y-%m-%d') THEN
+		SET NEW.hire_date = date_format(sysdate(), '%Y-%m-%d');
+		END IF;
+END $$ 
+DELIMITER ;
+
+INSERT employees
+VALUES (
+        '999904',
+        '1970-01-31',
+        'John',
+        'Johnson',
+        'M',
+        '2025-01-01'
+    );
+SELECT *
+FROM employees
+ORDER BY emp_no DESC; 
+
+-- INDEXES
+-- Drop the ‘i_hire_date’ index.
+ALTER TABLE employees
+DROP INDEX i_hire_date; 
+
+-- Similar to the exercises done in the lecture, obtain a result set containing the employee number, 
+-- first name, and last name of all employees with a number higher than 109990. Create a fourth column 
+-- in the query, indicating whether this employee is also a manager, according to the data provided in
+-- the dept_manager table, or a regular employee. 
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+        ELSE 'Employee'
+    END AS is_manager
+FROM employees e
+    LEFT JOIN dept_manager dm ON dm.emp_no = e.emp_no
+WHERE e.emp_no > 109990;
+
+-- Extract a dataset containing the following information about the managers: employee number, 
+-- first name, and last name. Add two columns at the end – one showing the difference between 
+-- the maximum and minimum salary of that employee, and another one saying whether this salary 
+-- raise was higher than $30,000 or NOT.
+USE employees;
+
+SELECT dm.emp_no,
+    e.first_name,
+    e.last_name,
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    CASE
+        WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary was raised by more then $30,000'
+        ELSE 'Salary was NOT raised by more then $30,000'
+    END AS salary_raise
+FROM dept_manager dm
+    JOIN employees e ON e.emp_no = dm.emp_no
+    JOIN salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+
+SELECT dm.emp_no,
+    e.first_name,
+    e.last_name,
+    MAX(s.salary) - MIN(s.salary) AS salary_difference,
+    IF(
+        MAX(s.salary) - MIN(s.salary) > 30000,
+        'Salary was raised by more then $30,000',
+        'Salary was NOT raised by more then $30,000'
+    ) AS salary_increase
+FROM dept_manager dm
+    JOIN employees e ON e.emp_no = dm.emp_no
+    JOIN salaries s ON s.emp_no = dm.emp_no
+GROUP BY s.emp_no;
+
+-- Extract the employee number, first name, and last name of the first 100 employees, and add a fourth column, 
+-- called “current_employee” saying “Is still employed” if the employee is still working in the company, or
+--  “Not an employee anymore” if they aren’t.
+
+SELECT 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    CASE
+        WHEN MAX(de.to_date) > SYSDATE() THEN 'Is still employed'
+        ELSE 'Not an employee anymore'
+    END AS current_employee
+FROM
+    employees e
+        JOIN
+    dept_emp de ON de.emp_no = e.emp_no
+GROUP BY de.emp_no
+LIMIT 100;
