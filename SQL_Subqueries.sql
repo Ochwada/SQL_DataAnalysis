e0339709a1f7d07b633b8366fc949d7dabae28a2
Ochwada <Ochwada@users.noreply.github.com>
2022-04-30 19:11:35 +0200

    UNION

--- SQL_Subqueries.sql
+++ SQL_Subqueries.sql
@@ -50,7 +50,7 @@ LIMIT 10;
 -- SQL Subqueries NESTED IN SELECT and FROM
 -- EXERCISE
 -- Assign employee number 110022 as a manager to all employees from 10001 to 10020, and employee number
---  11022 as a manager to all employees from 10021 to 10040
+--  110039 as a manager to all employees from 10021 to 10040
 SELECT A.*
 FROM (
         SELECT e.emp_no AS employee_ID,
@@ -66,5 +66,126 @@ FROM (
         WHERE e.emp_no <= 10020
         GROUP BY e.emp_no
         ORDER BY e.emp_no
-    ) as A;
+    ) as A
+UNION
+SELECT B.*
+FROM (
+        SELECT e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (
+                -- 
+                SELECT emp_no
+                FROM dept_manager
+                WHERE emp_no = 110039
+            ) AS manager_ID
+        FROM employees e
+            JOIN dept_emp de ON e.emp_no = de.emp_no
+        WHERE e.emp_no > 10020
+        GROUP BY e.emp_no
+        ORDER BY e.emp_no
+        LIMIT 10
+    ) as B;
 
+-- Starting your code with “DROP TABLE”, create a table called “emp_manager” 
+--(emp_no – integer of 11, not null; 
+--dept_no – CHAR of 4, null; manager_no – integer of 11, not null). 
+DROP TABLE IF EXISTS emp_manager;
+
+CREATE TABLE emp_manager(
+    emp_no int(11) NOT NULL,
+    dept_no char(4) NULL,
+    manager_no int (11) NOT NULL
+);
+
+-- --Fill emp_manager with data about employees, the number of the department they are working in, 
+-- and their managers.
+
+-- Your query skeleton must be:
+-- Insert INTO emp_manager SELECT 
+-- U.*
+-- FROM
+--                (A)
+-- UNION (B) UNION (C) UNION (D) AS U;
+
+-- A and B should be the same subsets used in the last lecture (SQL Subqueries Nested in SELECT and FROM)
+--. In other words, assign employee number 110022 as a manager to all employees from 10001 to 10020 (this 
+--must be subset A), and employee number 110039 as a manager to all employees from 10021 to 10040 (this must
+-- be subset B).
+
+--Use the structure of subset A to create subset C, where you must assign employee number 110039 as a 
+--manager to employee 110022.
+
+-- Following the same logic, create subset D. Here you must do the opposite - assign employee 110022 as 
+--a manager to employee 110039.
+
+-- Your output must contain 42 rows.
+
+INSERT INTO emp_manager
+SELECT u.*
+FROM (
+        SELECT a.*
+        FROM (
+                SELECT e.emp_no AS employee_ID,
+                    MIN(de.dept_no) AS department_code,
+                    (
+                        SELECT emp_no
+                        FROM dept_manager
+                        WHERE emp_no = 110022
+                    ) AS manager_ID
+                FROM employees e
+                    JOIN dept_emp de ON e.emp_no = de.emp_no
+                WHERE e.emp_no <= 10020
+                GROUP BY e.emp_no
+                ORDER BY e.emp_no
+            ) AS a
+        UNION
+        SELECT b.*
+        FROM (
+                SELECT e.emp_no AS employee_ID,
+                    MIN(de.dept_no) AS department_code,
+                    (
+                        SELECT emp_no
+                        FROM dept_manager
+                        WHERE emp_no = 110039
+                    ) AS manager_ID
+                FROM employees e
+                    JOIN dept_emp de ON e.emp_no = de.emp_no
+                WHERE e.emp_no > 10020
+                GROUP BY e.emp_no
+                ORDER BY e.emp_no
+                LIMIT 20
+            ) AS b
+        UNION
+        SELECT c.*
+        FROM (
+                SELECT e.emp_no AS employee_ID,
+                    MIN(de.dept_no) AS department_code,
+                    (
+                        SELECT emp_no
+                        FROM dept_manager
+                        WHERE emp_no = 110039
+                    ) AS manager_ID
+                FROM employees e
+                    JOIN dept_emp de ON e.emp_no = de.emp_no
+                WHERE e.emp_no = 110022
+                GROUP BY e.emp_no
+            ) AS c
+        UNION
+        SELECT d.*
+        FROM (
+                SELECT e.emp_no AS employee_ID,
+                    MIN(de.dept_no) AS department_code,
+                    (
+                        SELECT emp_no
+                        FROM dept_manager
+                        WHERE emp_no = 110022
+                    ) AS manager_ID
+                FROM employees e
+                    JOIN dept_emp de ON e.emp_no = de.emp_no
+                WHERE e.emp_no = 110039
+                GROUP BY e.emp_no
+            ) AS d
+    ) as u;
+
+-- SQL Self Join
+-- Applied when a table must join itself (Combining rows OF A TABLE with other rows of same table)
